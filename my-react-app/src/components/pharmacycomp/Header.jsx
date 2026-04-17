567cbe35a92794ad2c5a0e0e26e64f52d9d59e47
beshr-slaih <beshr.ka2004@gmail.com>
2026-04-17 22:02:34 +0300

    creat header

---
+++ my-react-app/src/components/pharmacycomp/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FiSearch, FiBell, FiUser } from 'react-icons/fi';
+import styles from './PharmaBridge.module.css';
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <div className={styles.searchBar}>
+                <FiSearch className={styles.searchIcon} />
+                <input type="text" placeholder="Search product catalogue..." className={styles.searchInput} />
+            </div>
+            <div className={styles.headerRight}>
+                <button className={styles.iconBtn}>
+                    <FiBell />
+                    <span className={styles.badge}></span>
+                </button>
+                <button className={styles.iconBtn}>
+                    <FiUser />
+                </button>
+            </div>
+        </header>
+    );
+}
