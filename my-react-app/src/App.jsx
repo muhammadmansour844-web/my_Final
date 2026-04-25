99f330ff3405bb22502e3a968f1a99dad9335f0d
muhammadmansour844 <muhammadmansour844@example.com>
2026-04-25 12:31:20 +0300

    Update app

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import RegisterPage from './pages/RegisterPage'
 import AdminDash from './pages/AdminDash'
 import CompanyDash from './pages/CompanyDash'
 import PharmacyDash from './pages/PharmacyDash'
+import PaymentPage from './pages/PaymentPage'
 import ProductDetailLayout from './components/PharmacyComp/products/ProductDetailLayout'
 import ProductBrowsingLayout from './components/PharmacyComp/products/ProductBrowsingLayout'
 
@@ -18,10 +19,11 @@ function App() {
         <Route path="/admin" element={<AdminDash />} />
         <Route path="/company" element={<CompanyDash />} />
         <Route path="/pharmacy-dashboard" element={<PharmacyDash />} />
+        <Route path="/payment" element={<PaymentPage />} />
         <Route path="/product-details" element={<ProductDetailLayout />} />
         <Route path="/product-browsing" element={<ProductBrowsingLayout />} />
       </Routes>
-    
+
   )
 }
 
