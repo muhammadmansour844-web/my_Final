b41b9f9a6b08a624dda1bfda2afc8b699ba19663
muhammadmansour844 <muhammadmansour844@example.com>
2026-04-25 12:12:49 +0300

    Update database config

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'final_js'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT)
 });
-
 module.exports = pool;
